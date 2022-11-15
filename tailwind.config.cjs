e9e2d9afc50d3b26209ef824238e33a888304db4
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-11-15 16:56:34 +0530

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  },
+  plugins: [],
+};
