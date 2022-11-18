5e05ee9d695ffd10b4dab1759ae06d013378a540
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-11-18 14:01:20 +0530

    navbar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Menu from "./Menu.json";
 import DevFest from "../../assets/DevFest.svg";
+import close from "../../assets/close.svg";
+import menu from "../../assets/menu.svg";
 
 export default function Nav() {
   const [scroll, setScroll] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -25,7 +28,9 @@ export default function Nav() {
             alt="DevFest Patna 2022"
             className="w-[40%] sm:w-[40%] inline-block"
           />
-          <span className="font-googleBold text-4xl ml-2">| Patna</span>
+          <span className="font-googleBold text-2xl lg:text-4xl ml-2">
+            | Patna
+          </span>
         </div>
         <ul className="hidden lg:flex justify-evenly">
           {Menu.map((item, id) => (
@@ -41,6 +46,37 @@ export default function Nav() {
             </li>
           ))}
         </ul>
+
+        <div
+          onClick={() => setToggle(!toggle)}
+          className="lg:hidden flex fles-1 justify-end items-center"
+        >
+          <img src={toggle ? menu : close} className="animate-bounce" />
+          <div
+            className={`fixed right-1 top-[3rem] p-2 rounded bg-[rgba(0,0,0,0.6)] backdrop-blur-[4px] z-50 ${
+              toggle ? "hidden" : "flex"
+            }`}
+          >
+            <ul
+              className={`list-none relative ${
+                toggle ? "hidden" : "flex"
+              } flex flex-col justify-center item-end`}
+            >
+              {Menu.map((item, id) => (
+                <li className="mx-1 my-2" key={id}>
+                  <a
+                    className={`min-w-full text-white whitespace-nowrap ${
+                      id === 0 ? "bg-primary" : ""
+                    } px-4 py-2 rounded-full hover:bg-secondary`}
+                    href={item.url}
+                  >
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
