43dbc8a03155bb0d961ac095fe4373c3b865cb13
pankaj shrivastava <shrivastava74795@gmail.com>
2022-11-19 17:02:34 +0530

    added speakers

--- src/Pages/Speakers/Speakers.jsx
+++ src/Pages/Speakers/Speakers.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Speaker from "../../Components/speaker/Speaker";
 import "./speakers.css";
+
 
 export default function Speakers() {
   return (
-    <div id="speakers" className="pt-12">
-      <h1 className="text-primary">Speakers</h1>
+    <div id="speakers"  className="mt-10">
+      <h1 className="text-secondary">Speakers</h1>
+      <div class=" mt-10 flex items-center w-screen min-h-screen">
+  <div class="container ml-auto mr-auto flex flex-wrap items-start">
+    <Speaker/>
+  </div>
+</div>
     </div>
   );
 }
