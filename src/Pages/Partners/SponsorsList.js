f04ba166a5874614256c574fa63a6ed7ceec0150
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-12-06 16:23:15 +0530

    added jetbrains

--- src/Pages/Partners/SponsorsList.js
+++ src/Pages/Partners/SponsorsList.js
@@ -1,8 +1,14 @@
-import { googleDevelopers } from "../../assets/SponsorsLogo";
+import { googleDevelopers, JetBrains } from "../../assets/SponsorsLogo";
 
 export const Sponsors = [
   {
     name: "Google Developers",
     logo: googleDevelopers,
+    url: "https://developers.google.com/",
+  },
+  {
+    name: "JetBrains",
+    logo: JetBrains,
+    url: "https://www.jetbrains.com/",
   },
 ];
--- src/assets/SponsorsLogo/index.js
+++ src/assets/SponsorsLogo/index.js
@@ -1,3 +1,4 @@
 import googleDevelopers from "./googleDevelopers.png";
+import JetBrains from "./JetBrains.svg";
 
-export { googleDevelopers };
+export { googleDevelopers, JetBrains };
